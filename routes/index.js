0dd1c98750fcf74e9bc4556a3337af7371a8f295
NTA2K <theanhqm@gmail.com>
2022-07-29 17:23:13 +0700

    lab5

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
+var multer = require('multer');
+var upload = multer({
+  dest: 'uploads', storage: storage})
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+file.originalname)
+  }
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express',
@@ -18,6 +30,13 @@ router.get('/blank',function (req,res){
 router.get('/buttons',function (req,res){
   res.render('buttons')
 })
+router.post('/uploads',upload.array('file'),function(req,res) {
+
+  res.json(req.files)
+
+});
+
+
 router.get('/404',function (req,res){
   res.render('404')
 })
